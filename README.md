313c9531a3ec91a358b92262b99abee8e0f234b0
Yandongxu <nextocean@gmail.com>
2016-10-13 18:49:11 +0800

    async components by default(unnamed)

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 - vue@2
 - vue-router@2
+- vuex-router-sync@3
 - webpack(code spliting)
+
+
+- Async component <https://github.com/JounQin/test/blob/c9bf63d33bb4e2be89aa8ea1857570901acc338a/vue/src/main.js>
--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,36 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import Foo from '../components/views/Foo.vue';
-import Bar from '../components/views/Bar.vue';
-import Other from '../components/views/Other.vue';
+// NOTE: sync components
+// import Foo from '../components/views/Foo.vue';
+// import Bar from '../components/views/Bar.vue';
+// import Other from '../components/views/Other.vue';
+
+// NOTE: Lazy load async components
+const Foo = resolve => require(['../components/views/Foo.vue'], resolve);
+const Bar = resolve => require(['../components/views/Bar.vue'], resolve);
+const Other = resolve => require(['../components/views/Other.vue'], resolve);
 
 Vue.use(Router);
 
-const routes = [
-  {
-    name: 'foo',
-    path: '/foo',
-    component: Foo
-  },
-  {
-    name: 'bar',
-    path: '/bar',
-    component: Bar
-  },
-  {
-    path: '*',
-    component: Other
-  }
-];
-
 export default new Router({
-  // mode: 'history',
-  // scrollBehavior: () => ({ y: 0 }),
-  routes
+  // hash|history
+  mode: 'hash',
+  scrollBehavior: () => ({ y: 0 }),
+  routes: [
+    {
+      name: 'foo',
+      path: '/foo',
+      component: Foo
+    },
+    {
+      name: 'bar',
+      path: '/bar',
+      component: Bar
+    },
+    {
+      path: '*',
+      component: Other
+    }
+  ]
 });
